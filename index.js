70d9b7819b46a9275812699787653724e498f373
Aditya <Adi.shah0912@gmail.com>
2020-03-09 22:55:52 +0530

    Added static folder

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!!<h1>');
-});
+//res.render -> Html template : pug,ejs
+//res.send -> send text, html code
+//res.sendfile -> Send file
+//res.json -> send json
+
+//static server : Dont need to route individually
+//No need to write routes for each html file
+
+/* app.get('/', (req, res) => {
+    res.sendfile(path.join(__dirname,'public','index.html'));
+}); */
+
+//Set static folder
+//After adding as static
+//We can go to localhost:5000/about.html Without routing
+//We can even add css files to static folder
+//Use is used whenever we want to include middleware
+app.use(express.static(path.join(__dirname,'public')));
+
 
 const PORT = process.env.PORT || 5000;
 
